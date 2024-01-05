8504bc12555eb0a910c39d9c1b5c2203da9c6be2
Ricky <henryats@gmail.com>
2024-01-05 18:54:52 +0000

    working Post from frontend to backend with redirection when successfull and messages when not

--- django/ft_transcendence/account/views/views.py
+++ django/ft_transcendence/account/views/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from ..forms import CreateUserForm
 
 def loginView(request):
     registerForm = CreateUserForm()
-    
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            
         
     context = {
         'registerForm': registerForm,
@@ -17,9 +11,24 @@ def loginView(request):
     return render(request, "account/login.html", context)
 
 
+
 def logoutView(request):
     return render(request, "account/logout.html")
 
 
+
 def registerView(request):
-    return render(request, "account/register.html")
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'status': 'success'})
+        else:
+            print('Not valid form')
+            errors = [str(error) for error in form.errors.values()]
+            return JsonResponse({'status': 'error', 'errors': errors})
+    else:
+        registerForm = CreateUserForm()
+        context = {'registerForm': registerForm}
+        return render(request, "account/login.html", context)
