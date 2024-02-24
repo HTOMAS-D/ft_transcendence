64cbc0857ac944e45a072fb1c3d519b0be531828
Diederik Boeren <diederik.boeren@gmail.com>
2024-02-24 14:44:14 +0100

    Profile page now loads user info

--- frontend/scripts/userInfo.js
+++ frontend/scripts/userInfo.js
@@ -1,10 +1,8 @@
 function getUserInfo()
 {
-    fetch("http://localhost:8082/user", {credentials: "include"}).then((res) => {
-        console.log(res);
+    return fetch("http://localhost:8082/user", {credentials: "include"}).then((res) => {
         return res.json();
     }).then((data) => {
-        console.log("data: " + JSON.stringify(data));
         sessionStorage.setItem("UserInfo", JSON.stringify(data));
     });
 }
