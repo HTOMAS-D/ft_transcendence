b372084798b5fa7905b87a053c2e015ed29483c6
Diederik Boeren <diederik.boeren@gmail.com>
2024-02-24 17:06:07 +0100

    Added pfp to database

--- django/share/models/models.py
+++ django/share/models/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from pathlib import Path
+
+DEFAULT_PFP_PATH = Path(__file__).parent / 'default_pfp.txt'
+DEFAULT_PFP = None
+
+with DEFAULT_PFP_PATH.open() as file:
+    DEFAULT_PFP = file.read()
 
 # Create your models here.
 class User(models.Model):
@@ -8,6 +15,7 @@ class User(models.Model):
     password = models.CharField(max_length=64)
     email = models.CharField(max_length=128, unique=True)
     username = models.CharField(max_length=16, unique=True)
+    pfp = models.TextField(null=False, default=DEFAULT_PFP)
     totp_secret = models.CharField(max_length=16) # Base 32 random 80 bit string
 
     class Meta:
